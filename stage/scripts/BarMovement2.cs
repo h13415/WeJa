1b60663d815815989aae842d2afbe9eeb2d5631b
Haruki Sakai <h.s.13415.b@gmail.com>
2018-09-15 12:59:01 +0900

    modified scripts

---
+++ stage/scripts/BarMovement2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarMovement2 : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	void Start () {
+		
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+
+		rb.velocity = new Vector3(7 * Mathf.Cos(Time.time), 0, 0);
+	}
+	
+}
